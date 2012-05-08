1017ab590a15bfe55c57864b9e46ed5ed8dd4158
ryosuke <krp87930@yahoo.co.jp>
2012-05-09 00:15:40 +0900

    First version.

---
+++ Sunfish/sunfish.cpp
@@ -0,0 +1,14 @@
+/*
+ * sunfish.cpp
+ *
+ *  Created on: 2012/05/08
+ *      Author: ryosuke
+ */
+
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+	std::cout << "Hello Sunfish!!\n";
+	return 0;
+}
+
