68546d3eb228f6b7168c43237d650ca6d3cabc44
Ryosuke <kubo@salamander.(none)>
2012-09-09 14:11:42 +0900

    remove Tools::Debug::Print method.

--- Sunfish/src/Tools/debug.cpp
+++ Sunfish/src/Tools/debug.cpp
@@ -25,37 +25,6 @@ namespace Tools{
 	using namespace Evaluates;
 	using namespace Search;
 
-	void Debug::Print( const char* str, ... ){
-		std::ofstream file;
-		char buf[1024];
-		time_t t;
-		struct tm* tmDate;
-		char strDate[64];
-		boost::xtime xt;
-		boost::xtime_get(&xt, boost::TIME_UTC);
-
-		va_list argx;
-		va_start( argx, str );
-
-		// ファイルオープン
-		file.open( "debug.txt", std::ios::out | std::ios::app );
-		if( file.is_open() ){
-			// 時刻
-			t = time( NULL );
-			tmDate = localtime( &t );
-			strftime( strDate, sizeof(strDate), "%F %H:%M:%S ", tmDate );
-
-			// 出力文字列
-			vsprintf( buf, str, argx );
-
-			// 出力
-			file << strDate << buf;
-			file.close();
-		}
-
-		va_end( argx );
-	}
-
 	bool Debug::PositionOk(const Position pos) {
 		// hash
 		if (pos.getHash() != pos.generateHash()) {
