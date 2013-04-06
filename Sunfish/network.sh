fe92e51e471263ad2114d72e4d70d134d99ab9a1
Ryosuke <kubo@salamander.(none)>
2013-04-06 16:58:31 +0900

    通信対局起動スクリプト

---
+++ Sunfish/network.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# 通信対局モード起動スクリプト
+#
+
+# TODO: 相対パス除去
+
+LOGFILE=network.log
+
+if [ -e ${LOGFILE} ]; then
+	mv ${LOGFILE} ${LOGFILE}.`date "+%Y%m%d-%H%M%S"`
+	echo mv ${LOGFILE} ${LOGFILE}.`date "+%Y%m%d-%H%M%S"`
+fi
+
+./sunfish -n&
